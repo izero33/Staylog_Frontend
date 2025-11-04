f10d8fc3a8fbd0e862d0f486043562d5e25761aa
youngseok <gys329@naver.com>
2025-11-04 14:36:42 +0900

    [추가, 수정] 예약 상세 페이지 모달 추가 / 코드 수정

--- staylog-frontend/src/domain/admin/pages/AdminReservationPage.tsx
+++ staylog-frontend/src/domain/admin/pages/AdminReservationPage.tsx
@@ -1,22 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { RESERVATION_STATUS, type AdminReservation, type ReservationStatus,} from "../types/AdminReservationTypes";
+import { type AdminReservation} from "../types/AdminReservationTypes";
 import { useEffect, useState } from "react";
 import { formatKST } from "../../../global/utils/date";
 import api from "../../../global/api";
+import AdminReservationDetailModal from "../components/AdminReservationDetailModal";
 
 
 
 
 function AdminReservationPage() {
 
+
+    // 예약 목록 상태
     const [reservations, setReservations] = useState<AdminReservation[]>([]);
+    // 상세 모달 상태
+    const [detailOpen, setDetailOpen] = useState(false);
+    const [targetBookingId, setTargetBookingId] = useState<number | null>(null);
+    // 로딩 / 에러 메시지 상태 
     const [loading, setLoading] = useState(true);
     const [errorMsg, setErrorMsg] = useState<string | null>(null);
-
-     // 취소요청 모달 상태
-    const [modalOpen, setModalOpen] = useState(false);
-    const [target, setTarget] = useState<AdminReservation | null>(null);
-    const [saving, setSaving] = useState(false);
 
     // 목록 조회
       useEffect(() => {
@@ -41,18 +43,23 @@ function AdminReservationPage() {
         };
       }, []);
 
-        // 상태 변경
-        async function patchStatus(bookingId:number, status:ReservationStatus) {
-        // 인터셉터로 data언래핑 가정
-        return api.patch(`/v1/admin/reservations/${bookingId}/status`, null, { params: { status } });
-    }
+      // 예약 상세 모달 * 이름 클릭 → 상세 모달 열기
+    const openDetail = (bookingId: number) => {
+    setTargetBookingId(bookingId);
+    setDetailOpen(true);
+  };
+  // 예약 상세 모달 닫기
+    const closeDetail = () => {
+    setDetailOpen(false);
+    setTargetBookingId(null);
+  };
 
 
   return (
     <div className="container-fluid py-3">
         <h1>예약 관리 페이지</h1>
         <div className="table-responsive">
-                  {/* 로딩 / 에러 상태 */}
+        {/* 로딩 / 에러 상태 */}
         {loading && (
         <div className="d-flex align-items-center gap-2 text-muted">
             <div className="spinner-border spinner-border-sm" role="status" />
@@ -61,35 +68,49 @@ function AdminReservationPage() {
         )}
         {!loading && errorMsg && (
         <div className="alert alert-danger" role="alert">
-            {errorMsg}
+        {errorMsg}
         </div>
       )}
-            <table className="table table-striped align-middle">
-                <thead className="table-light">
-                    <tr>
-                        <th>회원이름</th>
-                        <th>숙소명</th>
-                        <th>예약일</th>
-                        {/* <th>결제일</th> */}
-                        <th>체크인</th>
-                        <th>체크아웃</th>
-                        <th>예약 상태</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {reservations.map((res) => (
-                        <tr key={res.bookingId}>
-                            <td>{res.userName ?? res.guestName ?? "—"}</td>
-                            <td>{res.accommodationName ?? res.roomName ?? "—"}</td>
-                            <td>{formatKST(res.createdAt)}</td>
-                            <td>{formatKST(res.checkIn)}</td>
-                            <td>{formatKST(res.checkOut)}</td>
-                            <td>{res.status}</td>
-                        </tr>
-                    ))}
-                </tbody>
+          <table className="table table-striped align-middle">
+              <thead className="table-light">
+                  <tr>
+                      <th>회원이름</th>
+                      <th>숙소명</th>
+                      <th>예약일</th>
+                      {/* <th>결제일</th> */}
+                      <th>체크인</th>
+                      <th>체크아웃</th>
+                      <th>예약 상태</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  {reservations.map((res) => (
+                      <tr key={res.bookingId}>
+                          <td>
+                            <button 
+                            type="button"
+                            className="btn btn-link p-0 text-decoration-none"
+                            onClick={() => openDetail(res.bookingId)}
+                            title="상세 보기"
+                          >
+                            {res.userName ?? res.guestName ?? "—"}
+                            </button></td>
+                          <td>{res.accommodationName ?? res.roomName ?? "—"}</td>
+                          <td>{formatKST(res.createdAt)}</td>
+                          <td>{formatKST(res.checkIn)}</td>
+                          <td>{formatKST(res.checkOut)}</td>
+                          <td>{res.status}</td>
+                      </tr>
+                  ))}
+              </tbody>
             </table>
         </div>
+          {/* 상세 모달 */}
+      <AdminReservationDetailModal
+        open={detailOpen}
+        bookingId={targetBookingId}
+        onClose={closeDetail}
+      />
     </div>
   );
 }

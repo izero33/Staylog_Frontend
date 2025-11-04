eb9c3a1f35e4f7bbf5df53b645f70003c8ac230e
infreeJ <kasious97@naver.com>
2025-11-04 14:36:25 +0900

    [수정] EventSourcePolyfill로 SSE 구조 변경

--- staylog-frontend/src/domain/notification/hooks/useNotificationInitializer.ts
+++ staylog-frontend/src/domain/notification/hooks/useNotificationInitializer.ts
@@ -3,13 +3,14 @@ import type { AppDispatch } from "../../../global/store";
 import type { RootState } from "../../../global/store/types";
 import { useEffect } from "react";
 import api from "../../../global/api";
+import { EventSourcePolyfill } from "event-source-polyfill";
 
 
 /**
  * 최상위 컴포넌트에서 사용할 SSE 연결 훅
  * @author 이준혁
  */
-let eventSource: EventSource | null = null;
+let eventSource: EventSourcePolyfill | null = null;
 function useNotificationInitializer() {
 
    const dispatch = useDispatch<AppDispatch>();
@@ -52,27 +53,36 @@ function useNotificationInitializer() {
 
       // 토큰이 있는데 아직 연결이 안 됐다면
       if (token && !eventSource) {
-
-         // Bearer 제거
-         const pureToken = token ? token.substring(7) : '';
          console.log('SSE Hook 연결 시도...');
 
-         const es = new EventSource(`api/v1/notification/subscribe?token=${pureToken}`);
+         const es = new EventSourcePolyfill(`api/v1/notification/subscribe`, {
+            headers: {
+               'Authorization': token
+            },
+            heartbeatTimeout: 120000 // 2분마다 Heartbeat
+         });
          eventSource = es; // 인스턴스 저장
 
          es.onopen = () => console.log('SSE Hook 연결 성공');
-         es.onerror = () => {
-            console.error('SSE Hook 에러 발생, 연결 종료');
-            es.close();
-            eventSource = null;
-            // (여기서 자동 재연결을 막거나, 재시도 로직을 넣을 수 있음)
+
+         // Polyfill은 error 객체에서 HTTP status를 확인할 수 있다.
+         es.onerror = (error) => {
+            // @ts-ignore 타입 예외 처리
+            if (error.status === 401 || error.status === 403) {
+               console.error('[SSE Hook] 인증 실패 (401/403). 재연결 중지.');
+               es.close(); // 토큰이 만료됐을 때는 재시도를 막는다.
+               eventSource = null;
+            } else {
+               console.error('SSE Hook 에러 발생, 자동 재연결 시도...', error);
+            }
          };
 
          // 새로운 알림 추가 시 발생하는 이벤트
          es.addEventListener('new-notification', (event) => {
             try {
-               const newNoti = JSON.parse(event.data);
-               
+               const messageEvent = event as MessageEvent;
+               const newNoti = JSON.parse(messageEvent.data);
+
                // 새로운 알림 추가 액션
                dispatch({
                   type: 'PUSH_NOTIFICATION',

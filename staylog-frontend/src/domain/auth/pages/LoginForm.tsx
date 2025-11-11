7668404b9840aa013477a7f20a510a2ff51f4bb1
infreeJ <kasious97@naver.com>
2025-11-11 12:19:33 +0900

    [수정] 네비바, 로그인폼, 회원가입폼 디자인 변경

--- staylog-frontend/src/domain/auth/pages/LoginForm.tsx
+++ staylog-frontend/src/domain/auth/pages/LoginForm.tsx
@@ -132,7 +132,7 @@ function LoginForm({ onClose }: LoginFormProps = {}) {
       <div className="login-form-container">
         {/* 제목 섹션 */}
         <div className="login-header">
-          <h1 className="login-title">LOGIN</h1>
+          <h1 className="login-title fs-3 fw-normal text-dark" style={{ letterSpacing: '0.4rem' }}>LOGIN</h1>
           <p className="login-subtitle">로그인</p>
           <div className="login-divider"></div>
         </div>

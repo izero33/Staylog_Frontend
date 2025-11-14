2ad4faa4a74f90f385e7b6a5b705f0573e908d22
infreeJ <kasious97@naver.com>
2025-11-14 16:53:20 +0900

    [수정] 폰트 및 네비바 색상 변경

--- staylog-frontend/src/main.tsx
+++ staylog-frontend/src/main.tsx
@@ -7,6 +7,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "react-datepicker/dist/react-datepicker.css";
 import store from './global/store'
 import { Provider } from 'react-redux'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
 
